1e6edb6836cce44ff06d5a89acaf08aac2ce19f9
John9xDoe <felix9fox328forest@gmail.com>
2025-07-29 01:11:08 +0300

    feat: add CrossEntropy loss function

--- losses.py
+++ losses.py
@@ -1,8 +1,17 @@
 import numpy as np
 
 class CrossEntropy:
-    def forward(self, y_pred, y_true):
-        return -np.log(y_true)
-    def backward(self):
-        pass
+    def forward(self, probs, y_true):
+        self.probs = probs
+        self.y_true = y_true
+        batch_size = y_true.shape[0]
 
+        correct_logprobs = -np.log(probs[np.arange(batch_size), y_true])
+        return np.mean(correct_logprobs)
+
+    def backward(self):
+        batch_size = self.y_true.shape[0]
+        grad = self.probs.copy[0]
+        grad[np.arange(batch_size), self.y_true] -= 1
+        grad /= batch_size
+        return grad
